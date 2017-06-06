1e8e5c14869d3999791d73078c4d39b0344bab78
Jon Buda <jon.buda@gmail.com>
2017-06-06 13:31:21 -0500

    Rate service v20. handle certain errors more gracefully

--- lib/fedex/request/rate.rb
+++ lib/fedex/request/rate.rb
@@ -26,7 +26,7 @@ module Fedex
             "#{api_response["Fault"]["detail"]["fault"]["reason"]}\n--#{api_response["Fault"]["detail"]["fault"]["details"]["ValidationFailureDetail"]["message"].join("\n--")}"
           end rescue $1
 
-          raise RateError, response[:fault][:detail][:desc]
+          raise RateError, error_message || response[:fault][:detail][:desc]
         end
       end
 
